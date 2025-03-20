5dc582c13c72c082b30561e5a8d85eef6439a125
justachillguy57 <ghost_d2@proton.me>
2025-03-20 18:43:14 +0530

    add all files

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+pub mod meths;
+fn main() {
+    run();
+}
+
+fn run() {
+    let cmd_matches = meths::cmd::get_cmd();
+}
---
+++ src/meths/cmd.rs
@@ -0,0 +1,36 @@
+use clap::{Arg, ArgAction, ArgMatches, Command};
+
+pub fn get_cmd() -> ArgMatches {
+    let cmd = Command::new("stowrs")
+        .arg(
+            Arg::new("pth")
+                .help("Path argument")
+                .value_name("PATH")
+                .required(true),
+        )
+        .arg(
+            Arg::new("in-dir")
+                .help("where to search for the file mentioned [DEFAULT: `$HOME`]")
+                .value_name("DIR")
+                .required(false)
+                .short('i')
+                .long("in-dir"),
+        )
+        .arg(
+            Arg::new("check")
+                .help("check if the directories are able to be linked properly")
+                .required(false)
+                .short('C')
+                .long("check"),
+        )
+        .arg(
+            Arg::new("adopt")
+                .help("adopt existing files")
+                .required(false)
+                .short('o')
+                .long("adopt")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+    cmd
+}
